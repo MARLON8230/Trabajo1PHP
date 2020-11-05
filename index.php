d339bc3a2f4705b8311e43cfe4914801c36b60d0
Marlon Andres Castaño Uribe <pelecha610@gmail.com>
2020-11-05 00:02:24 -0500

    Se crea archivo index.php y se utilizan etiquetas HTML y hoja de estilos en CSS

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Perfumería VAMA</title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+</head>
+<body>
+
+    <header>
+        <div id="logo">
+            <img src="img/logo.jpg" target="Logo Vama" class="logo">
+        </div>
+    </header>
+
+    <h1>Bienvenidos a Perfumería VAMA</h1>
+
+    <section>
+
+        <div id="caja1">
+            <p id="frase">
+                ¡Todos vibramos con nuestra propia esencia, 
+                descubre la tuya, nosotros te asesoramos!. 
+                Las mejores lociones, al mejor precio.
+            </p>
+        </div>
+        <div id="caja2">
+            <p id="frase2">
+                ¡Hay perfumes que huelen a recuerdos!
+                Aquí te ofrecemos uno acorde a lo que quieres
+                expresar.
+            </p>
+        </div>
+        <div id="caja3">
+
+        </div>
+        <div id="caja4">
+            
+        </div>
+
+    </section>
+
+    <footer>
+       <img id="pie" src="img/footer.png" >
+    </footer>
+
+    <div id="redes">
+        <ul>
+            <li id="Facebook"><a href=" https://www.facebook.com/profile.php?id=100671684887662&ref=br_rs"><img src="img/facebook.png" alt="Facebook"></a></li>
+            <li id="Instagram"><a href="https://www.instagram.com/lociones_vama/"><img src="img/instagram.png" alt="Instagram"></a></li>
+            <li id="Whatsapp"><a href="https://wa.me/message/BSUHXYUMU64UA1"><img src="img/whatsapp.png" alt="Whatsapp"></a></li>
+
+        </ul>
+    </div>
+
+   
+    
+</body>
+</html>
